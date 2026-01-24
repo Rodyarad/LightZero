5e94f8444858e790e4efc4b5e6e2a9f3235f1529
Rodyarad <r.vakhitov2018@gmail.com>
2026-01-24 18:56:06 +0300

    add test train bash scripts

---
+++ test.sh
@@ -0,0 +1,7 @@
+export COMET_API_KEY=dummy #w0tT74tPZRbShiTpj5jG8ObBp
+export COMET_WORKSPACE='rodyarad'
+export COMET_PROJECT_NAME='lightzero'
+export CUDA_VISIBLE_DEVICES=0
+export CUDA_LAUNCH_BLOCKING=1
+
+python3 -u zoo/shapes2d/config/shapes2d_unizero_config_test.py
