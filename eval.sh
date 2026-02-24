331ba97ceab9d0108839fb79519e46e077bd2f11
Rodyarad <r.vakhitov2018@gmail.com>
2026-02-24 15:50:40 +0300

    add eval script for shapes2d

---
+++ eval.sh
@@ -0,0 +1,8 @@
+export COMET_API_KEY=dummy
+export COMET_WORKSPACE='rodyarad'
+export COMET_PROJECT_NAME='lightzero'
+export CUDA_VISIBLE_DEVICES=0
+export CUDA_LAUNCH_BLOCKING=1
+export PYTHONFAULTHANDLER=1
+
+python -u zoo/shapes2d/entry/shapes2d_eval.py
