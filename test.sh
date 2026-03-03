f2f692d38b36cdf77332e4ed56e4ac52bee90a76
Rodyarad <r.vakhitov2018@gmail.com>
2026-03-03 15:47:02 +0300

    added ocrl envs

--- test.sh
+++ test.sh
@@ -10,4 +10,5 @@ export CUDA_LAUNCH_BLOCKING=1
 #python3 -u zoo/robosuite/config/robosuite_pixels_suz_segment_config.py
 #python3 -u zoo/maniskill/config/maniskill_pixels_suz_segment_config.py
 #python3 -u zoo/vizdoom/config/vizdoom_unizero_segment_config.py
-python3 -u zoo/mof/config/mof_pixels_suz_segment_config.py
+#python3 -u zoo/mof/config/mof_pixels_suz_segment_config.py
+python3 -u zoo/ocrl/config/ocrl_unizero_segment_config_test.py
