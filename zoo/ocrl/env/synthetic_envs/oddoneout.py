301744a9e40b8f537bec0a370e1f16f6f45c4135
Rodyarad <r.vakhitov2018@gmail.com>
2026-03-24 16:23:31 +0300

    added own work seed for ocrl

--- zoo/ocrl/env/synthetic_envs/oddoneout.py
+++ zoo/ocrl/env/synthetic_envs/oddoneout.py
@@ -18,44 +18,44 @@ class OddOneOutEnv(BaseEnv):
 
     def _fill_properties(self, objs, unique_property, properties, idx):
         while sum(objs[:, idx] == 0) > 0:
-            prop = np.random.choice(properties)
+            prop = self.np_random.choice(properties)
             while prop == unique_property:
-                prop = np.random.choice(properties)
+                prop = self.np_random.choice(properties)
             if self._unseen_combi_mode is not None:
                 if idx == 0:
                     found = False
                     while not found:
                         if self._unseen_combi_mode == "train":
                             if prop == unique_property:
-                                prop = np.random.choice(properties)
+                                prop = self.np_random.choice(properties)
                                 found = False
                             elif unique_property == self._unseen_combi[0] and prop == self._unseen_combi[1]:
-                                prop = np.random.choice(properties)
+                                prop = self.np_random.choice(properties)
                                 found = False
                             elif unique_property == self._unseen_combi[1] and prop == self._unseen_combi[0]:
-                                prop = np.random.choice(properties)
+                                prop = self.np_random.choice(properties)
                                 found = False
                             else:
                                 found = True
                         elif self._unseen_combi_mode == "test":
                             if prop == unique_property:
-                                prop = np.random.choice(properties)
+                                prop = self.np_random.choice(properties)
                                 found = False
                             elif unique_property == self._unseen_combi[0] and prop == self._unseen_combi[1]:
                                 found = True
                             elif unique_property == self._unseen_combi[1] and prop == self._unseen_combi[0]:
                                 found = True
                             else:
-                                prop = np.random.choice(properties)
+                                prop = self.np_random.choice(properties)
                                 found = False
                         else:
                             raise ValueError
             #if idx == 0:  # to validate more objects odd-one-out-N?C3S1S1
             #    num_assigned_objs = sum(objs[:, idx] == 0)
             #else:
-            num_assigned_objs = np.random.randint(2, sum(objs[:, idx] == 0) + 1)
+            num_assigned_objs = self.np_random.integers(2, sum(objs[:, idx] == 0) + 1)
             while num_assigned_objs > 0:
-                obj_idx = np.random.randint(len(objs))
+                obj_idx = self.np_random.integers(len(objs))
                 if objs[obj_idx, idx] == 0:
                     objs[obj_idx, idx] = prop
                     num_assigned_objs -= 1
@@ -70,7 +70,7 @@ class OddOneOutEnv(BaseEnv):
             #  to modify the property selections through the rule for the unseen combination test
             target_obj_idx = 0
         else:
-            target_obj_idx = np.random.randint(self._num_objects)
+            target_obj_idx = self.np_random.integers(self._num_objects)
         # Randomly select the unique property
         types = []
         if len(self._COLORS) > 1:  # we cannot select the unique from a set of one item
@@ -79,37 +79,37 @@ class OddOneOutEnv(BaseEnv):
             types.append("shape")
         if len(self._SCALES) > 1:
             types.append("scale")
-        rand_type = np.random.choice(types)
+        rand_type = self.np_random.choice(types)
         if rand_type == "color":
-            unique_property = np.random.choice(self._COLORS)
+            unique_property = self.np_random.choice(self._COLORS)
             if self._unseen_combi_mode == "test":
                 found = False
                 while not found:
                     if not unique_property in self._unseen_combi:
-                        unique_property = np.random.choice(self._COLORS)
+                        unique_property = self.np_random.choice(self._COLORS)
                     else:
                         found = True
             objs[target_obj_idx, 0] = unique_property
             if self._obj_comp:
-                shape = np.random.choice(self._SHAPES)
+                shape = self.np_random.choice(self._SHAPES)
                 objs[:-1,1] = shape
-                scale = np.random.choice(self._SCALES)
+                scale = self.np_random.choice(self._SCALES)
                 objs[:-1,2] = scale
         elif rand_type == "shape":
-            unique_property = np.random.choice(self._SHAPES)
+            unique_property = self.np_random.choice(self._SHAPES)
             objs[target_obj_idx, 1] = unique_property
             if self._obj_comp:
-                color = np.random.choice(self._COLORS)
+                color = self.np_random.choice(self._COLORS)
                 objs[:-1,0] = color
-                scale = np.random.choice(self._SCALES)
+                scale = self.np_random.choice(self._SCALES)
                 objs[:-1,2] = scale
         elif rand_type == "scale":
-            unique_property = np.random.choice(self._SCALES)
+            unique_property = self.np_random.choice(self._SCALES)
             objs[target_obj_idx, 2] = unique_property
             if self._obj_comp:
-                color = np.random.choice(self._COLORS)
+                color = self.np_random.choice(self._COLORS)
                 objs[:-1,0] = color
-                shape = np.random.choice(self._SHAPES)
+                shape = self.np_random.choice(self._SHAPES)
                 objs[:-1,1] = shape
         # Assign other properties
         objs = self._fill_properties(objs, unique_property, self._COLORS, 0)
