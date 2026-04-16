f4f5e0dd72e81d86e0694bc7d9c869227f3fde4a
Rodyarad <r.vakhitov2018@gmail.com>
2026-04-16 13:44:17 +0300

    updated configs, vizdoom and seeding

--- test.sh
+++ test.sh
@@ -11,4 +11,5 @@ export CUDA_LAUNCH_BLOCKING=1
 #python3 -u zoo/maniskill/config/maniskill_pixels_suz_segment_config.py
 #python3 -u zoo/vizdoom/config/vizdoom_unizero_segment_config.py
 #python3 -u zoo/mof/config/mof_pixels_suz_segment_config.py
-python3 -u zoo/ocrl/config/ocrl_unizero_segment_config_test.py
+#python3 -u zoo/ocrl/config/ocrl_unizero_segment_config_test.py\
+python3 -u zoo/vizdoom/config/vizdoom_unizero_segment_config.py
